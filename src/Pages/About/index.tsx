4c2db305baa796931347f262be87641150744131
Filipi Rafael <filipirafael.123@gmail.com>
2023-07-15 16:35:41 -0300

    changing about description and cv versino

--- src/Pages/About/index.tsx
+++ src/Pages/About/index.tsx
@@ -4,52 +4,57 @@ import {
   Title,
   Description,
   Avatar,
-  AboutLink
-} from "./style"
+  AboutLink,
+} from "./style";
 
 import { Navbar } from "../../Components/Navbar";
 import { Contact } from "../../Components/Contact";
 
-import AboutPicture from '../../assets/aboutpicture.svg';
+import AboutPicture from "../../assets/aboutpicture.svg";
 
 export const About = () => {
   return (
     <Container>
-      <Navbar actualPage='About' />
+      <Navbar actualPage="About" />
 
       <Wrapper>
-
-        <Avatar
-          src={AboutPicture}
-        />
-
+        <Avatar src={AboutPicture} />
         <Title>About</Title>
-
         <Description>
-          A Full-Stack Engineer that loves apply his knowledge to solve problems, create amazing products and impact lives. As a developer I seek to help my team to become more effective and less experienced developers to grow/learn more.
+          Hello! I'm Filipi, a passionate Full-Stack Engineer dedicated to
+          solving problems and creating impactful products. As a developer, I
+          seek to help my team to become more effective and less experienced
+          developers to grow/learn more. Currently working at BuiltGrid, I have
+          the privilege of developing innovative solutions that transform the
+          construction market in Australia.
         </Description>
-
         <Description>
-          I currently work at BuiltGrid, where I have the privilege of develop products that have impacts on millions of Australians, transforming how construction market works.
+          With expertise in JavaScript/TypeScript, I build web and mobile
+          applications using React and React Native. I strive to create
+          beautiful, user-friendly interfaces that provide real solutions.
+          Additionally, I excel in developing RESTful APIs using NodeJS, Prisma,
+          and various NoSQL and SQL solutions.
         </Description>
-
         <Description>
-          I develop solutions and ideas using my creativity and my technical knowledge in Javascript/Typescript building web-apps with React and mobile-apps using React Native and that aren't just beautiful, but enjoyable to use and really problem solvers. Also, I develop RESTful APIs using NodeJS, Prisma and other NoSQL and SQL solutions.
+          I love challenging myself with side projects, some of which you can
+          find on my website (filipirafael.com). I also share my knowledge and
+          insights through articles on my Medium profile (https://medium.com/
+          @filipirafael.123).
         </Description>
-
         <Description>
-          Studying English every day with native teachers to improve my communication skills and I have international experience as a front-end developer.
+          Driven by continuous growth, I study English daily with native
+          teachers to enhance my communication skills. Having gained
+          international experience as a front-end developer, I'm eager to
+          explore opportunities that allow me to make a global impact.
         </Description>
-
         <AboutLink
-          href='https://drive.google.com/file/d/1IjWUZqncIze3v25c3NDwjtxGHvjWz6GS/view?usp=sharing'
-          target='_blank'
+          href="https://drive.google.com/file/d/1HmaCIcHO-TZlnsm3uGmavS0n-77K5HH8/view?usp=share_link"
+          target="_blank"
         >
           View CV ↗︎
         </AboutLink>
-
         <Contact />
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
