dea8aa91147988e5ac876ff424ce0313fe3a4d2b
Filipi Rafael <filipirafael.123@gmail.com>
2023-07-16 19:56:35 -0300

    Adding a new article

--- src/mocks/articles.ts
+++ src/mocks/articles.ts
@@ -26,19 +26,26 @@ export const allArticles = [
   {
     id: 1,
     date: '2023',
+    title: 'Publishing My First App, Finally 🥳',
+    description: 'I’m thrilled to share the journey of creating and publishing my first mobile application, BeReady!',
+    link: 'https://medium.com/@filipirafael.123/publishing-my-first-app-finally-af3b4459e8bf'
+  },
+  {
+    id: 2,
+    date: '2023',
     title: 'How Embracing Design Transformed My Mobile Development Journey',
     description: 'In the ever-evolving world of app development, creating a successful mobile application requires more than just coding expertise.',
     link: 'https://medium.com/@filipirafael.123/how-embracing-design-transformed-my-mobile-development-journey-92f75b9eebff'
   },
   {
-    id: 2,
+    id: 3,
     date: '2023',
     title: 'Embark on a Language Learning Adventure: My Journey to English Fluency!',
     description: 'Today, I’m thrilled to share my exhilarating journey towards English fluency. Join me as we explore the methods, resources, and experiences that have shaped my language-learning adventure.',
     link: 'https://medium.com/@filipirafael.123/embark-on-a-language-learning-adventure-my-journey-to-english-fluency-2f0624d5b541'
   },
   {
-    id: 3,
+    id: 4,
     date: '2022',
     title: 'What to study to learn Front-end Development',
     description: 'I am writing this article to help some friends, to start on Front-end Development Career. In this article i wanna route you from scratch to web Framework.',
